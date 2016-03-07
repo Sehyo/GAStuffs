3067ac6b0673f17d1e3df23b5214e872b4ee5819
Sehyo <Alex_Noble@hotmail.com>
2016-03-07 01:43:29 +0000

    Added another way to crossover
    Added another way to crossover

--- GAAssignmentADjn/Genome.cpp
+++ GAAssignmentADjn/Genome.cpp
@@ -10,7 +10,8 @@
 typedef unsigned int uint;
 
 #define MUTATION_RATE 10 // SET this somewhere between 0 - 100;
-#define MAX_NUMBER 100000 // Maximum number for a coefficient
+#define MAX_NUMBER 500 // Maximum number for a coefficient
+#define BINARY_CROSS 0
 
 std::random_device Genome::rd;
 std::mt19937 Genome::mt(rd());
@@ -74,11 +75,70 @@ void Genome::mutate()
 
 Genome Genome::crossover(Genome& otherGenome)
 {	
+
 	Genome loveChild(data);
-	for(int i = 0; i < genes.size(); i++)
+	if(BINARY_CROSS == 1)
 	{
-		if(randomInt(0,100) < 50) loveChild.genes.at(i) = genes.at(i);
-		else loveChild.genes.at(i) = otherGenome.genes.at(i);
+		typedef union
+		{
+			double value;
+			char bits[sizeof(double)];
+		} Bits;
+
+		Bits a1, b1, c1, d1, e1, f1;
+		Bits a2, b2, c2, d2, e2, f2;
+		Bits a3, b3, c3, d3, e3, f3;
+		Bits temp;
+		a1.value = genes.at(0); b1.value = genes.at(1); c1.value = genes.at(2); d1.value = genes.at(3); e1.value = genes.at(4); f1.value = genes.at(5);
+		a2.value = otherGenome.genes.at(0); b2.value = otherGenome.genes.at(1); c2.value = otherGenome.genes.at(2); d2.value = otherGenome.genes.at(3); e2.value = otherGenome.genes.at(4); f2.value = otherGenome.genes.at(5);
+
+		int crossPoint = randomInt(0, sizeof(double)*6);
+
+		for(int i = 0; i < sizeof(double) * 6; i++)
+		{
+				switch(int(i / sizeof(double))) // Find which Gene we are using.
+				{
+					case 0:
+						if(i < crossPoint)	a3.bits[i % sizeof(double)] = a1.bits[i % sizeof(double)];
+						else a3.bits[i % sizeof(double)] = a2.bits[i % sizeof(double)];
+						break;
+					case 1:
+						if(i < crossPoint)	b3.bits[i % sizeof(double)] = b1.bits[i % sizeof(double)];
+						else b3.bits[i % sizeof(double)] = b2.bits[i % sizeof(double)];
+						break;
+					case 2:
+						if(i < crossPoint)	c3.bits[i % sizeof(double)] = c1.bits[i % sizeof(double)];
+						else c3.bits[i % sizeof(double)] = c2.bits[i % sizeof(double)];
+						break;
+					case 3:
+						if(i < crossPoint)	d3.bits[i % sizeof(double)] = d1.bits[i % sizeof(double)];
+						else d3.bits[i % sizeof(double)] = d2.bits[i % sizeof(double)];
+						break;
+					case 4:
+						if(i < crossPoint)	e3.bits[i % sizeof(double)] = e1.bits[i % sizeof(double)];
+						else e3.bits[i % sizeof(double)] = e2.bits[i % sizeof(double)];
+						break;
+					case 5:
+						if(i < crossPoint)	f3.bits[i % sizeof(double)] = f1.bits[i % sizeof(double)];
+						else f3.bits[i % sizeof(double)] = f2.bits[i % sizeof(double)];
+						break;
+				}
+		}
+
+		loveChild.genes.at(0) = a3.value;
+		loveChild.genes.at(1) = b3.value;
+		loveChild.genes.at(2) = c3.value;
+		loveChild.genes.at(3) = d3.value;
+		loveChild.genes.at(4) = e3.value;
+		loveChild.genes.at(5) = f3.value;
+	}
+	else
+	{
+		for(int i = 0; i < genes.size(); i++)
+		{
+			if(randomInt(0,100) < 50) loveChild.genes.at(i) = genes.at(i);
+			else loveChild.genes.at(i) = otherGenome.genes.at(i);
+		}
 	}
 	loveChild.evaluateFitness();
 	return loveChild;	
